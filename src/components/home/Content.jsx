326bc0ca475d94b69828e77bbb651b2f07d058fa
arsen1c <vector2912@gmail.com>
2021-08-20 13:46:47 +0530

    added mock data for feed

--- src/components/home/Content.jsx
+++ src/components/home/Content.jsx
@@ -1,12 +1,36 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 
 export default function Content(props) {
+
+  const [data, setData] = useState(null);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    fetch('http://localhost:4000/posts')
+      .then(res => {
+        if (!res.ok) {
+          throw new Error('Could not fetch the resource');
+        }
+
+        return res.json();
+      })
+      .then(json => {
+        setData(() => json);
+        console.log(json);
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  }, []);
+
+  console.log('Data:', data);
+
   return (
     <div className="max-w-96 h-auto overflow-hidden">
-    <div className="mb-4 rounded border b-1 post-card md:w-96 bg-white justify-center pb-2">
+      <div className="mb-4 rounded border b-1 post-card md:w-96 bg-white justify-center pb-2">
         <div className="post-head flex items-center py-2 px-4">
-          <img 
-            src="https://cdn.discordapp.com/attachments/491113748031864842/502707407307407370/IMG_20180901_102050-2.jpg" 
+          <img
+            src="https://cdn.discordapp.com/attachments/491113748031864842/502707407307407370/IMG_20180901_102050-2.jpg"
             alt=""
             className="h-10 w-10 bg-contain overflow-hidden rounded-3xl"
           />
@@ -15,10 +39,10 @@ export default function Content(props) {
             arsen1c
           </div>
         </div>
-        <img 
-          src="https://cdn.discordapp.com/attachments/491113748031864842/502707407307407370/IMG_20180901_102050-2.jpg" 
-        alt=""
-          className="max-w-96 h-auto overflow-hidden" 
+        <img
+          src="https://cdn.discordapp.com/attachments/491113748031864842/502707407307407370/IMG_20180901_102050-2.jpg"
+          alt=""
+          className="max-w-96 h-auto overflow-hidden"
         />
         <div className="flex opacity-80 justify-between post-actions px-3 pt-2">
           <div className="main">
@@ -33,37 +57,6 @@ export default function Content(props) {
         </div>
         <span className="font-bold px-3">arsen1c</span>
         <span>New profile pic 🎉</span>
-      </div>
-      <div className="mb-4 rounded border b-1 post-card md:w-96 bg-white pb-2">
-        <div className="post-head flex items-center py-2 px-4">
-          <img 
-            src="https://cdn.discordapp.com/attachments/491113748031864842/502707407307407370/IMG_20180901_102050-2.jpg" 
-            alt=""
-            className="h-10 w-10 bg-contain overflow-hidden rounded-3xl"
-          />
-          {/* Make this a Link tag */}
-          <div className="username ml-4 font-bold">
-            arsen1c
-          </div>
-        </div>
-        <img 
-          src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.wallpapers13.com%2Fwp-content%2Fuploads%2F2018%2F11%2FThe-Magic-Islands-of-Lofoten-Norway-Europe-winter-morning-light-landscape-Desktop-HD-Wallpaper-For-PC-Tablet-And-Mobile-3840x2160-1920x1440.jpg&f=1&nofb=1" 
-          alt=""
-          className="max-w-96 h-auto overflow-hidden" 
-        />
-        <div className="flex opacity-80 justify-between post-actions px-3 pt-2">
-          <div className="main">
-            <i className="far fa-heart text-2xl mr-4 cursor-pointer"></i>
-            <i className="far fa-comment text-2xl mr-4 cursor-pointer"></i>
-            <i className="far fa-paper-plane text-2xl cursor-pointer"></i>
-          </div>
-          <i className="far fa-bookmark text-2xl cursor-pointer"></i>
-        </div>
-        <div className="likes font-bold px-3">
-          10 likes
-        </div>
-        <span className="font-bold px-3">arsen1c</span>
-        <span>Winter days are awesome 🎉🐛🐒</span>
       </div>
     </div>
   )
