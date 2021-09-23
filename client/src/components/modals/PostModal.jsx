ceef81361ae0ff0685ecf30eae56033686226a85
Arsenic <vector2912@gmail.com>
2021-09-23 17:11:43 +0530

    fix: image modal

--- client/src/components/modals/PostModal.jsx
+++ client/src/components/modals/PostModal.jsx
@@ -27,17 +27,15 @@ export default function Modal({ data }) {
       {showModal ? (
         <>
           <div
-            className="justify-center max-w-96 items-center flex overflow-x-hidden overflow-hidden fixed inset-0 z-50 outline-none focus:outline-none"
+            className="backdrop"
+            onClick={() => setShowModal(false)}
           >
-            <div className="mx-auto max-w-3xl">
-              {/*content*/}
+            {/*
+              <div className="mx-auto max-w-3xl">
               <div className="border-0 shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
-                {/*header*/}
-                {/*body*/}
                 <div className="p-2 ">
                   <img src={image} alt="" className="max-h-96"/>
                 </div>
-                {/*footer*/}
                 <div className="flex items-center justify-end">
                   <button
                     className="text-red-500 background-transparent font-bold uppercase px-6 py-2 text-sm outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
@@ -49,8 +47,9 @@ export default function Modal({ data }) {
                 </div>
               </div>
             </div>
+            */}
+            <img src={image} alt="" className=""/>
           </div>
-          <div className="opacity-25 fixed inset-0 z-40 bg-black"></div>
         </>
       ) : null}
     </>
